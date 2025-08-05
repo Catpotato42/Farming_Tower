855bbd29b9e443b7a8efb47393b26f84a3e9c3f2
Ian Stolte <stolte@usc.edu>
2025-08-05 15:36:07 -0700

    Cattail tower

---
+++ Source/Farming_Tower/Indicators/CattailIndicator.cpp
@@ -0,0 +1,14 @@
+#include "CattailIndicator.h"
+
+
+int ACattailIndicator::IsGoodPlacement()
+{
+    int riverDist = TowerPlacement->GetRiverDistance(GetActorLocation());
+    if (riverDist > 0) //decrease level if not in water
+        return -1;
+    else
+    {
+        float zPos = GetActorLocation().Z;
+        return zPos <= 550 ? 1 : 0; //better if lower
+    }
+}
